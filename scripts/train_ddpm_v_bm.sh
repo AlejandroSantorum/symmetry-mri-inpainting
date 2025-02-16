53f4f0604eef603e5449d398cb02d250ed8db0d4
AlejandroSantorum <alejandro.santorum@gmail.com>
2025-02-16 12:48:52 +0100

    Add bash scripts to train the three different ddp models

--- scripts/train_ddpm_v_bm.sh
+++ scripts/train_ddpm_v_bm.sh
@@ -7,8 +7,8 @@
 
 source /home/proyectos/ada2/santorum/venv_phd/bin/activate
 
-MODEL_NAME="ddpm_v"
-INPUT_CHANNELS=2
+MODEL_NAME="ddpm_v_bm"
+INPUT_CHANNELS=3
 TRAIN_SAMPLES=400
 MODEL_FLAGS="--image_size 256 --num_in_channels ${INPUT_CHANNELS} --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16"
 TRAIN_FLAGS="--save_interval 20000"
@@ -19,7 +19,7 @@ python3 /home/santorum/repos/symmetry-mri-inpainting/symmetry_mri_inpainting/tra
     --data_dir /scratch/santorum/data/IXI-skull-stripped-mni-dm-inpainting-preprocessed-3d \
     --output_dir "/scratch/santorum/checkpoints/${MODEL_NAME}_ixi_seed${TRAIN_SEED}" \
     --input_img_types "voided,mask,brain" \
-    --output_img_types "voided,brain" \
+    --output_img_types "voided,mask,brain" \
     --reference_img_type "mask" \
     --num_cutoff_samples $TRAIN_SAMPLES \
     --dataset_seed 42 \
