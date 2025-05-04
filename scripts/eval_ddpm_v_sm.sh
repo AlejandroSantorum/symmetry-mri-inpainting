82154486c743f48afbc2dd75d73dcc0e117a7412
AlejandroSantorum <alejandro.santorum@gmail.com>
2025-05-04 12:08:14 +0100

    Refactor training and evaluation bash scripts

--- scripts/eval_ddpm_v_sm.sh
+++ scripts/eval_ddpm_v_sm.sh
@@ -7,8 +7,14 @@
 
 source /home/proyectos/ada2/santorum/venv_phd/bin/activate
 
+# Model config
 MODEL_NAME="ddpm_v_sm"
 INPUT_CHANNELS=3
+INPUT_IMG_TYPES="voided,symm-mask,brain"
+OUTPUT_IMG_TYPES="voided,symm-mask,brain"
+REF_IMG_TYPE="symm-mask"
+
+# Training config
 NUM_CUTOFF_SAMPLES=30
 NUM_MAX_SAMPLES=30
 DATASET_SEED=42  # seed for the dataset split
@@ -18,6 +24,7 @@ CHECKPOINT_NAME="savedmodel080000"
 CHECKPOINT_DIR="/scratch/santorum/checkpoints/${MODEL_NAME}_ixi_seed${TRAIN_SEED}"
 EVAL_RESULTS_DIR="/scratch/santorum/inference/${MODEL_NAME}_ixi_seed${TRAIN_SEED}/${CHECKPOINT_NAME}"
 
+# Run the evaluation script
 PYTHONPATH="/home/santorum/repos/symmetry-mri-inpainting" \
 python3 /home/santorum/repos/symmetry-mri-inpainting/symmetry_mri_inpainting/evaluate.py \
     --data_dir /scratch/santorum/data/IXI-skull-stripped-mni-dm-inpainting-preprocessed-3d \
@@ -25,13 +32,13 @@ python3 /home/santorum/repos/symmetry-mri-inpainting/symmetry_mri_inpainting/eva
     --png_output_dir "${EVAL_RESULTS_DIR}/validation_slices_png" \
     --npy_output_dir "${EVAL_RESULTS_DIR}/validation_slices_npy" \
     --xlsx_output_dir $EVAL_RESULTS_DIR \
+    --input_img_types $INPUT_IMG_TYPES \
+    --output_img_types $OUTPUT_IMG_TYPES \
+    --reference_img_type $REF_IMG_TYPE \
+    --num_cutoff_samples $NUM_CUTOFF_SAMPLES \
+    --num_max_samples $NUM_MAX_SAMPLES \
     --model_image_size 256 \
     --actual_image_size 224 \
     --sample_batch_size 8 \
-    --input_img_types "voided,symm-mask,brain" \
-    --output_img_types "voided,symm-mask,brain" \
-    --reference_img_type "symm-mask" \
-    --num_cutoff_samples $NUM_CUTOFF_SAMPLES \
-    --num_max_samples $NUM_MAX_SAMPLES \
     --dataset_seed $DATASET_SEED \
     --evaluation_seed $EVAL_SEED
--- scripts/train_ddpm_v.sh
+++ scripts/train_ddpm_v.sh
@@ -7,21 +7,28 @@
 
 source /home/proyectos/ada2/santorum/venv_phd/bin/activate
 
+# Model config
 MODEL_NAME="ddpm_v"
 INPUT_CHANNELS=2
+INPUT_IMG_TYPES="voided,mask,brain"
+OUTPUT_IMG_TYPES="voided,brain"
+REF_IMG_TYPE="mask"
+
+# Training config
 TRAIN_SAMPLES=400
 MODEL_FLAGS="--image_size 256 --num_in_channels ${INPUT_CHANNELS} --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16"
 TRAIN_FLAGS="--save_interval 20000"
 DATASET_SEED=42  # seed for the dataset split
 TRAIN_SEED=42  # seed for training - other experiments used 10
 
+# Run the training script
 PYTHONPATH="/home/santorum/repos/symmetry-mri-inpainting" \
 python3 /home/santorum/repos/symmetry-mri-inpainting/symmetry_mri_inpainting/train.py \
     --data_dir /scratch/santorum/data/IXI-skull-stripped-mni-dm-inpainting-preprocessed-3d \
     --output_dir "/scratch/santorum/checkpoints/${MODEL_NAME}_ixi_seed${TRAIN_SEED}" \
-    --input_img_types "voided,mask,brain" \
-    --output_img_types "voided,brain" \
-    --reference_img_type "mask" \
+    --input_img_types $INPUT_IMG_TYPES \
+    --output_img_types $OUTPUT_IMG_TYPES \
+    --reference_img_type $REF_IMG_TYPE \
     --num_cutoff_samples $TRAIN_SAMPLES \
     --dataset_seed $DATASET_SEED \
     --training_seed $TRAIN_SEED \
